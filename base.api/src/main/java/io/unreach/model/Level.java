ab4987dd49b57726b11612eeb68b806aaf545fc6
joe jiao <yingjunjiao@outlook.com>
2018-03-14 21:07:52 +0800

    fix

--- base.api/src/main/java/io/unreach/model/Level.java
+++ base.api/src/main/java/io/unreach/model/Level.java
@@ -12,19 +12,25 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "等级模型")
 public class Level {
 
+  @ApiModelProperty(value = "主键")
   private String id = null;
 
+  @ApiModelProperty(value = "等级显示名称")
   private String name = null;
 
+  @ApiModelProperty(value = "等级描述")
   private String desc = null;
 
+  @ApiModelProperty(value = "等级程度")
+  private String degree;
+
+  @ApiModelProperty(value = "等级所属领域")
   private Career career = null;
 
 
   /**
    * 主键
    **/
-  @ApiModelProperty(value = "主键")
   public String getId() {
     return id;
   }
@@ -36,7 +42,6 @@ public class Level {
   /**
    * 等级显示名称
    **/
-  @ApiModelProperty(value = "等级显示名称")
   public String getName() {
     return name;
   }
@@ -48,7 +53,6 @@ public class Level {
   /**
    * 等级描述
    **/
-  @ApiModelProperty(value = "等级描述")
   public String getDesc() {
     return desc;
   }
@@ -60,12 +64,19 @@ public class Level {
   /**
    * 等级所属领域
    **/
-  @ApiModelProperty(value = "等级所属领域")
   public Career getCareer() {
     return career;
   }
 
   public void setCareer(Career career) {
     this.career = career;
+  }
+
+  public String getDegree() {
+    return degree;
+  }
+
+  public void setDegree(String degree) {
+    this.degree = degree;
   }
 }
