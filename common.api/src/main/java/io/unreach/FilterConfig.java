91069e461119e00927e73baf7787626f93f86646
joe jiao <yingjunjiao@outlook.com>
2018-03-20 17:07:47 +0800

    fix

--- common.api/src/main/java/io/unreach/FilterConfig.java
+++ common.api/src/main/java/io/unreach/FilterConfig.java
@@ -1,5 +1,6 @@
 package io.unreach;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
+  @Autowired
+  private SessionFilter sessionFilter;
+  @Autowired
+  private ApiOriginFilter apiOriginFilter;
+
   @Bean
   public FilterRegistrationBean apiOriginFilter() {
     FilterRegistrationBean registration = new FilterRegistrationBean();
-    registration.setFilter(new ApiOriginFilter());
+    registration.setFilter(apiOriginFilter);
     registration.addUrlPatterns("/*");
     registration.setName("ApiOriginFilter");
+
     registration.setOrder(1);
     return registration;
   }
@@ -26,11 +33,21 @@ public class FilterConfig {
   @Bean
   public FilterRegistrationBean sessionFilter() {
     FilterRegistrationBean registration = new FilterRegistrationBean();
-    registration.setFilter(new SessionFilter());
+    registration.setFilter(sessionFilter);
     registration.addUrlPatterns("/*");
     registration.setName("SessionFilter");
     registration.setOrder(2);
     return registration;
+  }
+
+  @Bean
+  public ApiOriginFilter initApi() {
+    return new ApiOriginFilter();
+  }
+
+  @Bean
+  public SessionFilter initSession() {
+    return new SessionFilter();
   }
 
 }
